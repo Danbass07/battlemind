7acbeb7f0d686a4b8e4bd9e31d2735beecb3f25c
Danbass07 <danbass666@googlemail.com>
2019-07-03 13:25:58 +0100

    api works number of players and hype

--- app/Http/Controllers/HypeController.php
+++ app/Http/Controllers/HypeController.php
@@ -27,22 +27,27 @@ class HypeController extends Controller
 
     public function typedetail($id, $tid)
   {
+    $thisType2= Type::find($tid)->load('users');
         $thisType= Type::find($tid);
-        Log::info($thisType);
         $group=Group::where('id', $id)->with(['users','users.types', 'users.players'])->get();
 
-        $totalHype = $group->pluck('users')->collapse()->map(function ($user) use ($tid) {
-            return    $user->types->map(function ($type) use ($tid ){    
-                            if($type->type == $tid) {
-                            return $type->pivot->hype;  
+        $totalHype = $thisType2->users->map(function ($user) use ($tid) {
+            $hype = 0;
+            return    $user->types->map(function ($type) use ($tid , $hype){    
+             
+                            if($type->id === intval($tid)) {
+                            return $hype +=  $type->pivot->hype;
                             }
-                            return 0;
+                            return ;
                         });
         })->collapse()->toArray();
 
+
+
+
         $numberOfPlayers = $group->pluck('users')->collapse()->map(function ($user) use ($tid ,$thisType) {
             return    $user->players->map(function ($player) use ($tid ,$thisType){    
-                Log::info($player);
+               
                             if($player->type == $thisType->type) {
                             return 1;  
                             }
@@ -54,6 +59,7 @@ class HypeController extends Controller
         $types=Type::all()->load('groups');
 
          return response()->json([
+             'thisType' => $thisType2,
              'group' =>$group[0],
              'type' => $type[0],
              'types' => $types,
